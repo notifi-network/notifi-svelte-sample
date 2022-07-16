603c187cd0998291acd15f3fb89de4fa1b08d46b
juni-notifi <hyungjoon.kim@notifi.network>
2022-07-16 12:23:58 -0700

    Add subscription route
    This route will be used to show the Subscription sample.
    
    Also implemented an unstyled <form> to be used in the sample.

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -16,11 +16,8 @@
 		</svg>
 		<ul>
 			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Todos</a>
+			<li class:active={$page.url.pathname === '/subscription'}>
+				<a sveltekit:prefetch href="/subscription">Subscription Sample</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
---
+++ src/routes/subscription/index.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    let emailInput = '';
+    let telegramInput = '';
+    let announcementsSubscribed = false;
+
+    const handleSubmit = () => {
+        console.log('Submit!', emailInput, telegramInput, announcementsSubscribed);
+    };
+</script>
+
+<svelte:head>
+    <title>Subscription</title>
+    <meta name="description" content="Demo of how to subscribe to notifi" />
+</svelte:head>
+
+<div class="subscriptions">
+    <h1>Subscription Sample</h1>
+
+    <form>
+        <fieldset>
+            <legend>Contact Information</legend>
+            <label for="email">Email Address</label>
+            <input type="email" name="email" bind:value={emailInput} />
+            <label for="telegram">Telegram ID</label>
+            <input type="text" name="telegram" bind:value={telegramInput} />
+        </fieldset>
+        <fieldset>
+            <legend>Active Subscriptions</legend>
+            <label for="announcements">Notifi Announcements</label>
+            <input type="checkbox" bind:checked={announcementsSubscribed} />
+        </fieldset>
+    </form>
+    <button on:click={handleSubmit}>Subscribe</button>
+</div>
